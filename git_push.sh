9088a61efdadb5199671a673828d6b7b02bcaf55
acanoe <imcanoe@gmail.com>
2013-09-23 15:34:54 +0800

    Linux java connect c

--- git_push.sh
+++ git_push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git init
 git add .
-git commit -m 'android phone manager program for ubuntu'
+git commit -m 'Linux java connect c'
 #git remote add origin git@github.com:acanoe/androidmanager.git
 git config --global user.email "imcanoe@gmail.com"
 git config --global user.name "acanoe"
